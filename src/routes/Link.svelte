8a3cd723d46ce0a6746445a353c2d692cf34c10a
DavutHaxor <37223772+DavutHaxor@users.noreply.github.com>
2022-06-07 23:59:17 +0300

    Add files via upload

---
+++ src/routes/Link.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { Base64 } from 'js-base64';
+	export let params = {};
+    let data = params.d;
+	let url = data.split(".")[0];
+	let uploader = data.split(".")[1];
+	let decrypted_url = Base64.decode(url);
+	let domain = decrypted_url.split("/")[2];
+
+
+	function redirect() {
+		window.location.href = decrypted_url;
+	}
+</script>
+
+<main>
+	<div class="container d-flex justify-content-center text-center">
+	<div class="container ">
+	{#if data.includes(".") && uploader.length > 0 }
+		<h3>Link Provided by <b>{Base64.decode(uploader)}</b></h3>
+		{:else}
+		<h3>Link Provided by Turkish Drive</h3>
+	{/if}
+	<a href="{decrypted_url}"><button class="btn btn-primary btn-lg"><i class="bi bi-download"></i> Download</button></a>
+	<br>
+	<small>This link will take you to <b>{domain}</b></small>
+</div>
+</div>
+</main>
